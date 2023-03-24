62c29e32537c7f88cfb065af7feea007618e7d13
Edgardo Ortiz Jr <edgardo.ortiz.jr11@gmail.com>
2023-03-24 10:53:52 -0500

    Annotations submission

---
+++ src/Author.java
@@ -0,0 +1,44 @@
+/**
+ * This source file is subject to the license that is bundled with this package in the file LICENSE.
+ */
+import java.util.ArrayList;
+import java.util.List;
+
+@SuppressWarnings({"all"})
+public class Author extends Person {
+
+    private final List books;
+
+    @SuppressWarnings({"all", "unchecked"})
+    public Author(String firstName, String lastName) {
+        super(firstName, lastName);
+        books = new ArrayList();
+    }
+
+    /**
+     * @deprecated Use publishedBooks instead
+     */
+
+
+    @Deprecated
+    @SuppressWarnings({ "all", "deprecated", "unchecked"})
+    public List<String> getBooks() {
+        return books;
+    }
+
+
+    @SuppressWarnings({"all", "unchecked"})
+    public List<String> publishedBooks() {
+        return books;
+    }
+
+    @SuppressWarnings({"all", "unchecked"})
+    public void addBook(String book) {
+        books.add(book);
+    }
+
+    @SuppressWarnings({"all"})
+    public String sortName() {
+        return String.format("%s, %s", lastName, firstName);
+    }
+}
